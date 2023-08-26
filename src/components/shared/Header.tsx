e55d8e2285c8a2e5b26732b8f53e34942f6e0aec
Jony Bhattacharjee <jonybhattacharjee886@gmail.com>
2023-08-26 12:31:40 +0600

    Authentication Done

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,31 +1,36 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
-
+import { useAuth, UserButton } from '@clerk/nextjs'
 const Header = () => {
+    const {userId} = useAuth()
   return (
-      <nav className='py-6 border-b border-red-300'>
-          <div className="container mx-auto">
-              <div className="flex justify-between items-center">
-                  <div className="flex items center gap-4">
-                      <h1>Logo</h1>
-                      <span>Search</span>
-                  </div>
-                  <div className="flex items-center gap-4">
-                      <div className="group">
-                          <Link href="/find-mentor">
-                              Find Mentor
-                          </Link>
-                      </div>
-                      <button>
-                          <Link href="/sign-in">
-                              Sign In
-                          </Link>
-                      </button>
-                    </div>
-              </div>
-          </div>
-    </nav>
-  )
+		<nav className="py-6 border-b border-red-300">
+			<div className="container mx-auto">
+				<div className="flex justify-between items-center">
+					<div className="flex items center gap-4">
+						<h1>Logo</h1>
+						<span>Search</span>
+					</div>
+					<div className="flex items-center gap-4">
+						<div className="group">
+							<Link href="/find-mentor">Find Mentor</Link>
+						</div>
+						{!userId ? (
+							<>
+								<button>
+									<Link href="/sign-in">Sign In</Link>
+								</button>
+							</>
+						) : (
+                              <>
+                              <UserButton afterSignOutUrl='/sign-in' /></>
+						)}
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Header
